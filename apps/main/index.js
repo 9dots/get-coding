ceee76546c3865f2ba69bbb825e54d4872ecbffa
Josh Taylor <joshrtay@gmail.com>
2014-11-14 20:16:45 -0800

    add sandbox

--- apps/main/index.js
+++ apps/main/index.js
@@ -8,7 +8,8 @@ angular.module(name, [
   'ui.router',
   'ngMaterial',
   require('/apps/space-man'),
-  require('/apps/space-man-blockly')
+  require('/apps/space-man-blockly'),
+  require('/apps/space-man-sandbox')
 ])
 .config(['$compileProvider', function($compileProvider) {
   var re = /^\s*(?:blob(?::|%3A))?(https?|ftp|file)(:|%3A)|data:image\//;
