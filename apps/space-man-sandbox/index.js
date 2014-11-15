f58dcfbd91f3b3ff0bf36e615510d155a932adee
Josh Taylor <joshrtay@gmail.com>
2014-11-14 20:20:17 -0800

    add sandbox

---
+++ apps/space-man-sandbox/index.js
@@ -0,0 +1,16 @@
+var name = module.exports = 'space-man-sandbox';
+
+angular.module(name, [
+  'ui.router', 
+  require('/lib/code-app'), 
+  require('/lib/workspace-ace'), 
+  require('/lib/playspace-spaceman')
+])
+.config(['appsProvider', function(appsProvider) {
+  appsProvider.register({
+    data: require('./package.json'),
+    workspace: require('./workspace.html'),
+    playspace: require('./playspace.html'),
+    levels: require('/lib/space-man-levels').practice
+  });
+}]);
