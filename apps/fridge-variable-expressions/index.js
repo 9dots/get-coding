fb795c1df6c9b9838701a303799eb0bcd5058a53
Ryan Baber <prbaber@gmail.com>
2015-01-13 17:29:55 -0800

    wip

--- apps/fridge-variable-expressions/index.js
+++ apps/fridge-variable-expressions/index.js
@@ -12,6 +12,28 @@ angular.module(name, [
     workspace: require('./workspace.html'),
     playspace: require('./playspace.html'),
     help: require('./README.md'),
-    levels: [] // specify levels here
+    levels: [
+      {"playspace":{
+        "directions": "These are the directions.",
+        "help":"This is the help.", 
+        "hint": "This is the hint.",
+        "drawings": [
+          {"id": "drawing1",
+            "class": "book", 
+            "labelid": "label1"
+          }, 
+          {"id": "drawing2", 
+            "class": "seahorse", 
+            "labelid": "label2"
+          }
+        ], 
+        "variables": ["word1", "word2", "word3", "word4"], 
+        "values": ["horse", "book", "sea", "fish"]
+      }, "workspace":{
+        "codespace": ""
+      }
+    }
+
+    ] // specify levels here
   });
 }]);
